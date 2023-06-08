ca441c591fb5fdb1499db3cc7018eef4d0c5c7fb
walidEssaied <walidessaied01@gmail.com>
2023-06-08 20:36:47 +0100

    update alert payment

---
+++ src/extensions/users-permissions/controllers/User.js
@@ -0,0 +1,25 @@
+// users/me with media and relational fields
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = (user) =>
+  sanitizeEntity(user, {
+    model: strapi.query("user", "users-permissions").model,
+  });
+
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const userQuery = await strapi.query("user", "users-permissions");
+    const userWithMedia = await userQuery.findOne({ id: ctx.state.user.id });
+
+    const data = sanitizeUser(userWithMedia, { model: userQuery.model });
+    ctx.send(data);
+  },
+};
